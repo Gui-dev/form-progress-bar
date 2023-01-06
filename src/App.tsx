1c3a0b3c5dc7856d2f0ba74a22f2abbae06a96d5
Gui-dev <adrianon10@gmail.com>
2023-01-06 04:27:05 -0300

    feat: add helper functions handleChangeValue and calculateProgress

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,101 @@
+import { ChangeEvent, ChangeEventHandler, FormEvent, SelectHTMLAttributes, useEffect, useState } from 'react'
+
 import './styles/style.css'
 
+type DataProps = {
+  fullname: string
+  email: string
+  marital_status: string
+  genre: string
+}
+
 function App () {
+  const [data, setData] = useState<DataProps>({
+    fullname: '',
+    email: '',
+    marital_status: '',
+    genre: ''
+  })
+
+  const handleChangeValue = (event: ChangeEvent<HTMLInputElement | HTMLSelectElement>) => {
+    event.preventDefault()
+    const { name, value } = event.target
+    setData(prev => {
+      const newData = { ...prev, [name]: value }
+      return newData
+    })
+  }
+
+  const calculateProgress = (): number => {
+    let value = 0
+    const amountToAdd = 25
+    if (data.fullname) value += amountToAdd
+    if (data.email) value += amountToAdd
+    if (data.marital_status) value += amountToAdd
+    if (data.genre) value += amountToAdd
+
+    return value
+  }
 
   return (
     <div className='App'>
       <h1>Progresso Do Formulário</h1>
 
       <main>
-        {/* crie a barra de progresso aqui */}
+        <div className="bar-container">
+          <div className="bar" style={{ width: `${calculateProgress()}%` }}></div>
+        </div>
         <div className='form-group'>
           <label htmlFor=''>Nome Completo</label>
-          <input />
+          <input
+            type="text"
+            name="fullname"
+            value={data.fullname}
+            onChange={handleChangeValue}
+          />
         </div>
         <div className='form-group'>
           <label htmlFor=''>E-mail</label>
-          <input />
+          <input
+            type="email"
+            name="email"
+            value={data.email}
+            onChange={handleChangeValue}
+          />
         </div>
         <div className='form-group'>
           <label htmlFor=''>Estado Civil</label>
-          <select>
+          <select
+            name="marital_status"
+            value={data.marital_status}
+            onChange={handleChangeValue}
+          >
             <option value=''>- selecione...</option>
-            <option value='solteiro'>Solteiro</option>
-            <option value='casado'>Casado</option>
-            <option value='divorciado'>Divorciado</option>
+            <option value='single'>Solteiro</option>
+            <option value='married'>Casado</option>
+            <option value='divorced'>Divorciado</option>
           </select>
         </div>
         <div className='form-group'>
           <label htmlFor=''>Gênero</label>
           <div className='radios-container'>
             <span>
-              <input type='radio' /> Masculino
+              <input
+                type='radio'
+                name="genre"
+                value="male"
+                onChange={handleChangeValue}
+                checked={data.genre === 'male'}
+              /> Masculino
             </span>
             <span>
-              <input type='radio' /> Feminino
+              <input
+                type='radio'
+                name="genre"
+                value="female"
+                onChange={handleChangeValue}
+                checked={data.genre === 'female'}
+              /> Feminino
             </span>
           </div>
         </div>
